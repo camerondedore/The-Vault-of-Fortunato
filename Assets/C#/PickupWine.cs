67b992b5e21e265e12640885737eb7aac10234ae
unknown <camerondedore@gmail.com>
2021-08-22 15:39:06 -0700

    adding wine

---
+++ Assets/C#/PickupWine.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupWine : MonoBehaviour, IPickup
+{
+    
+
+
+
+
+	void FixedUpdate()
+    {
+        transform.Rotate(Vector3.up, 90 * Time.fixedDeltaTime, Space.World);
+    }
+
+
+
+	public void Pickup(Transform player)
+	{
+		// heal code here
+		Destroy(gameObject);
+	}
+}
