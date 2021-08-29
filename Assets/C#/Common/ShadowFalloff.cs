8cc183a3ce9ac25d3f0b1e8b1f01180b15b402be
unknown <camerondedore@gmail.com>
2021-08-28 17:00:00 -0700

    working on inn

---
+++ Assets/C#/Common/ShadowFalloff.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShadowFalloff : MonoBehaviour
+{
+    
+	[Tooltip("create different falloff curves for each quality level")]
+	[SerializeField]
+	AnimationCurve[] qualityfalloffCurves;
+	Transform mainCamera;
+	Light lamp;
+
+
+
+    void Start()
+    {
+		mainCamera = Camera.main.transform;
+		lamp = GetComponent<Light>();
+    }
+
+
+
+    void Update()
+    {
+		var distaneToCamera = Vector3.Distance(transform.position, mainCamera.position);
+		var falloff = qualityfalloffCurves[QualitySettings.GetQualityLevel()].Evaluate(distaneToCamera);
+        lamp.shadowStrength = falloff;
+
+		if(falloff <= 0 && lamp.shadows != LightShadows.None)
+		{
+			lamp.shadows = LightShadows.None;
+		}
+
+		if(falloff > 0 && lamp.shadows == LightShadows.None)
+		{
+			lamp.shadows = LightShadows.Hard;
+		}
+    }
+}
