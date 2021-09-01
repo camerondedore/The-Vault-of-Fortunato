c41dd709ba1766b41dacacece2d69c938d806215
unknown <camerondedore@gmail.com>
2021-09-01 08:31:03 -0700

    starting player brick collecting

---
+++ Assets/C#/PlayerBricks.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerBricks : MonoBehaviour
+{
+    
+	public static int bricksCollected = 0;
+	[SerializeField]
+	Text brickCounter;
+	int oldBrickCount = 0;
+
+
+
+	void Update()
+	{
+		if(oldBrickCount != bricksCollected)
+		{
+			oldBrickCount = bricksCollected;
+			brickCounter.text = $"Bricks: {bricksCollected}/18";
+		}
+	}
+
+
+
+	public static void AddBrick()
+	{
+		bricksCollected++;
+	}
+}
