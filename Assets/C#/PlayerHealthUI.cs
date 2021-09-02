ddf6efdeeec07cd50c9b1ba8a50460dc1612b2f9
unknown <camerondedore@gmail.com>
2021-09-01 21:37:57 -0700

    adding ui heart animation

--- Assets/C#/PlayerHealthUI.cs
+++ Assets/C#/PlayerHealthUI.cs
@@ -16,6 +16,32 @@ public class PlayerHealthUI : MonoBehaviour
 
 	public void Update()
 	{
+		ShowOrHideHearts();
+
+		var index = 0;
+		foreach(var h in hearts)
+		{
+			if(index <= health.hitPoints - 1)
+			{
+				// animate heart
+				var speed = health.hitPoints == 3 ? 0.1f : health.hitPoints == 2 ? 0.25f : 1f;
+				var phase = index / health.maxHitPoints;
+				var scale = 1 + Mathf.PingPong(Time.time * speed + phase, 0.25f);
+				h.transform.localScale = Vector3.one * scale;
+			}
+			else
+			{
+				break;
+			}
+
+			index++;
+		}
+	}
+
+
+
+	public void ShowOrHideHearts()
+	{
 		// update UI
 		if(oldHitPoints != health.hitPoints)
 		{
