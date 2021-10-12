bd7e467e25859e2991f6331cdaf2af5b90572c5f
unknown <camerondedore@gmail.com>
2021-10-11 19:54:47 -0700

    working on menu

---
+++ Assets/C#/Menu/MenuButtons.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuButtons : MonoBehaviour
+{
+    
+
+
+
+
+	public void Play()
+	{
+		// set player's starting inn location to the cellar
+		InnDataManager.data.startAtDoor = false;
+		InnDataManager.SaveData();
+
+		SceneLoader.LoadLevel("Inn");
+	}
+
+
+
+	public void Quit()
+	{
+		Application.Quit();
+	}
+
+
+
+	public void ClearSavedData()
+	{
+		// clear bricks and health from save file
+		PlayerDataManager.data = new PlayerDataManager.PlayerData();
+		PlayerDataManager.SaveData();
+	}
+}
