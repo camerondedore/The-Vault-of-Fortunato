c7ddfb42414e50d97db069cc9534e082122c3e5b
unknown <camerondedore@gmail.com>
2021-10-13 19:59:06 -0700

    working on menu

---
+++ Assets/C#/Intro/IntoButtons.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntoButtons : MonoBehaviour
+{
+   
+
+
+
+
+	public void SkipIntro()
+	{
+			SceneLoader.LoadLevel("Menu");
+	}
+}
---
+++ Assets/C#/Intro/IntroText.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntroText : MonoBehaviour
+{
+    [SerializeField]
+	string[] textSegments;
+	//int
+
+
+
+
+}
+
+
+
+/*
+Fortunato had not favored me.  How brutish he is, to soil my name.  Of all that I have tolerated while keeping quiet.  No more...
+
+He was a self-centered count.  His eyes only followed his greed.  Never enough.  But this clouded judgement would be easy to exploit.  Never a second thought when clutching new wealth...
+
+I met him outside my establishment.  Yes, I had sent him an invitation to sample my newest fine wines.  Brought in from the famed town of Montilla and rumored to even mend the sick and injured! ...
+
+He was halfway through the door when he turned his on back long enough for my instrument of revenge to strike his unconscious.  My plan was in motion, and fate itself was set in stone...
+
+Fortunato's plump figure was more difficult to pull down into the cellar than I had anticipated.  His cold damp grave would wait.  It had nowhere of more import to be...
+
+I tossed him over a small mound of bricks into an old vault, just large enough to fit the vile man.  Each brick added lifted my spirit.  Plonk, plonk, plonk; The job will be done before the sun rises...
+
+I reached blindly to the stack of masonry.  I grabbed nothing but the dank air and almost fell over.  No more bricks!  I had to be quick, there must be bricks somewhere around here! I cannot allow Fortunato's rotten stench to ruin my wine.
+*/
